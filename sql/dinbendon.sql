9d0af7599876c72d6f604d0b6a7ff5a5b29a6892
Kento <rogeraabbccdd@gmail.com>
2018-11-06 16:01:25 +0800

    更新sql檔
    修復匯入錯誤

--- sql/dinbendon.sql
+++ sql/dinbendon.sql
@@ -1,50 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.8.1
--- https://www.phpmyadmin.net/
---
--- 主機: 127.0.0.1
--- 產生時間： 2018-10-19 10:09:46
--- 伺服器版本: 10.1.33-MariaDB
--- PHP 版本： 7.1.18
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- 資料庫： `aaa`
---
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `class`
---
-
 CREATE TABLE `class` (
   `id` int(11) NOT NULL,
   `name` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- 資料表的匯出資料 `class`
---
-
 INSERT INTO `class` (`id`, `name`) VALUES
-(2, '測試'),
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `menu`
---
+(1, '測試');
 
 CREATE TABLE `menu` (
   `id` int(10) NOT NULL,
@@ -52,12 +12,6 @@ CREATE TABLE `menu` (
   `name` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `price` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `orders`
---
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
@@ -68,12 +22,6 @@ CREATE TABLE `orders` (
   `note` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- 資料表結構 `restaurant`
---
-
 CREATE TABLE `restaurant` (
   `id` int(10) NOT NULL,
   `name` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -83,12 +31,6 @@ CREATE TABLE `restaurant` (
   `menu` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- 資料表結構 `review`
---
-
 CREATE TABLE `review` (
   `id` int(11) NOT NULL,
   `stu_num` int(11) NOT NULL,
@@ -96,12 +38,6 @@ CREATE TABLE `review` (
   `review` int(11) NOT NULL,
   `comment` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- 資料表結構 `student`
---
 
 CREATE TABLE `student` (
   `id` int(11) NOT NULL,
@@ -112,96 +48,45 @@ CREATE TABLE `student` (
   `num` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- 資料表的匯出資料 `student`
---
-
 INSERT INTO `student` (`id`, `number`, `pass`, `name`, `class`, `num`) VALUES
-(1, '1234', 1234, '測試', 2, 0),
-(2, '5678', 5678, '測試', 2, 0),
+(1, '1234', 1234, '測試', 1, 0),
+(2, '5678', 5678, '測試', 1, 0);
 
---
--- 已匯出資料表的索引
---
 
---
--- 資料表索引 `class`
---
 ALTER TABLE `class`
   ADD PRIMARY KEY (`id`);
 
---
--- 資料表索引 `menu`
---
 ALTER TABLE `menu`
   ADD PRIMARY KEY (`id`);
 
---
--- 資料表索引 `orders`
---
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
---
--- 資料表索引 `restaurant`
---
 ALTER TABLE `restaurant`
   ADD PRIMARY KEY (`id`);
 
---
--- 資料表索引 `review`
---
 ALTER TABLE `review`
   ADD PRIMARY KEY (`id`);
 
---
--- 資料表索引 `student`
---
 ALTER TABLE `student`
   ADD PRIMARY KEY (`id`);
 
---
--- 在匯出的資料表使用 AUTO_INCREMENT
---
 
---
--- 使用資料表 AUTO_INCREMENT `class`
---
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- 使用資料表 AUTO_INCREMENT `menu`
---
 ALTER TABLE `menu`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=413;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
---
--- 使用資料表 AUTO_INCREMENT `orders`
---
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=447;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
---
--- 使用資料表 AUTO_INCREMENT `restaurant`
---
 ALTER TABLE `restaurant`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
---
--- 使用資料表 AUTO_INCREMENT `review`
---
 ALTER TABLE `review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
---
--- 使用資料表 AUTO_INCREMENT `student`
---
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
