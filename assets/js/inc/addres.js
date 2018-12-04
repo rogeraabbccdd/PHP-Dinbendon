6ef6c94a413ab47918d1e8a12443e4005b1dcdbe
Kento <rogeraabbccdd@gmail.com>
2018-12-04 16:17:54 +0800

    更新css、js和字體
    更新版型檔案到Bootstrap 4版本

---
+++ assets/js/inc/addres.js
@@ -0,0 +1,142 @@
+		// Add new menu
+		$('#newmenub').on('click', function() {
+			$('.form-group').removeClass('has-error');
+			$('#modalname').val("");
+			$('#modalprice').val("");
+			$('#modal_form').modal('show');
+		});
+		
+		// Add new menu to datatables
+		$('#savemenum').on('click', function() {
+			// get input value
+			var name = $('#modalname').val();
+			var price = $('#modalprice').val();	
+			
+			// check empty
+			if(name && price)
+			{
+				// add to rows
+				var r1 = "<input type='text' value='"+name+"' name='name2[]' class='namemenu'>";
+				var r2 = "<input type='text' value='"+price+"' name='price2[]' class='pricemenu'>";
+				var r3 = "<button type='button' class='delmenu2 btn btn-danger btn-simple'><i class='material-icons'>close</i></button>";
+				var rowadd = menutable.row.add([r1, r2, r3]).draw().node();
+				// hide modal
+				$('#modal_form').modal('hide');
+			}
+			else
+			{
+				swal({
+				  title: '錯誤',
+				  text: '欄位不能為空',
+				  type: 'error',
+				  confirmButtonClass: "btn btn-info",
+				  buttonsStyling: false
+				})
+			}
+		});
+		
+		// 刪除菜單
+		$(document).on("click", ".delmenu", function() {
+			if( $(this).closest("td").find("i.material-icons").text() == "close")
+			{
+				// check
+				$(this).closest("tr").find("input[name='del[]']").attr("checked", true);
+				// change color
+				$(this).closest("tr").css("background-color", "pink");
+				// change button
+				$(this).removeClass("btn-danger");
+				$(this).addClass("btn-success");
+				$(this).html("<i class='material-icons'>undo</i>");
+			}
+			else
+			{
+				// uncheck
+				$(this).closest("tr").find("input[name='del[]']").attr("checked", false);
+				// change color
+				$(this).closest("tr").css("background-color", "white");
+				// change button
+				$(this).removeClass("btn-success");
+				$(this).addClass("btn-danger");
+				$(this).html("<i class='material-icons'>close</i>");
+			}
+		});
+		
+		$(document).on("click", ".delmenu2", function() {
+			menutable.row( $(this).closest("tr") ).remove().draw();
+		});
+		
+		// 保存
+		$("#saveres").on("click", function(e){
+			if($(".namemenu").length > 0 && $("input[name='resname']").val() && $("input[name='tel']").val())
+			{
+				$("#saveres").attr("disabled", true);
+				$("#saveres").val("保存中");
+			
+				var fd = new FormData( $("#resform").get(0) ); 
+				menutable.$('input').each(function(){
+					if($(this).attr("type") == "checkbox")
+					{
+						if( $(this).is(":checked") )
+							fd.append( $(this).attr("name"), $(this).val() );
+					}
+					else	fd.append( $(this).attr("name"), $(this).val() );
+				});
+				
+				$.ajax({
+					type: 'post',
+					url: './assets/inc/addres.php<?=((!empty($id))?"?id=".$id:"")?>',
+					data: fd,
+					cache: false,
+					contentType: false,
+					processData: false,
+					success: function(r){
+						console.log(r);
+						if(r == "large")
+						{
+							swal({
+							  title: '錯誤',
+							  text: '圖片大於1MB',
+							  type: 'error',
+							  confirmButtonClass: "btn btn-info",
+							  buttonsStyling: false
+							})
+						}
+						else if(r == "today")
+						{
+							swal({
+							  title: '錯誤',
+							  text: '不能編輯今日餐廳',
+							  type: 'error',
+							  confirmButtonClass: "btn btn-info",
+							  buttonsStyling: false
+							})
+						}
+						else if(r == "ok")
+						{
+							swal({
+								title: '成功!',
+								text: '新增成功',
+								type: 'success',
+								confirmButtonClass: "btn btn-success",
+								buttonsStyling: false
+							}).then(function () {
+								window.location.href="./reslist.php";
+							})
+						}
+					}
+				})
+				
+				$("#saveres").attr("disabled", false);
+				$("#saveres").val("保存");
+			}
+			else
+			{
+				swal({
+				  title: '錯誤',
+				  text: '菜單和餐廳名稱、電話不能為空',
+				  type: 'error',
+				  confirmButtonClass: "btn btn-info",
+				  buttonsStyling: false
+				})
+			}
+		})
---
+++ assets/js/inc/loginform.js
@@ -0,0 +1,131 @@
+	$('#LoginButton').on('click', function() {
+		$('#loginform')[0].reset();
+		$('.form-group').removeClass('has-error');
+		$('.help-block').empty();
+		$('#modal_form').modal('show');
+	});
+	
+	var loginform = $('#loginform');
+		loginform.validate({
+			rules: {
+				loginnumber: { required: true},
+				loginpass: { required: true, minlength: 4, minlength: 4},
+			},
+			errorPlacement: function(error, element) {
+				$(element).parent('div').addClass('has-error');
+			},
+		});
+		
+	loginform.on('submit', function(e) {
+		e.preventDefault();
+		if(loginform.valid())
+		{
+			var val = $('#loginnumber').val();
+			var val2 = $('#loginpass').val();
+			$('#SubmitLogin').text('登入中...');
+			$('#SubmitLogin').attr('disabled',true);
+			$.ajax({
+				type: "post",
+				url: "./assets/inc/auth.php",
+				data: {	login: val, pass: val2},
+				dataType: "text",
+				success: function(r) {
+					console.log(r);
+					if(r == "success")
+					{
+						swal({
+							title: '成功!',
+							text: '成功登入.',
+							type: 'success',
+							confirmButtonClass: "btn btn-success",
+							buttonsStyling: false
+						}).then(function () {
+							window.location.href="./reslist.php";
+						})
+					}
+					else if(r == "failed")
+					{
+						swal({
+						  title: '登入失敗',
+						  text: '帳號或密碼錯誤',
+						  type: 'error',
+						  confirmButtonClass: "btn btn-info",
+						  buttonsStyling: false
+						});
+						$('#SubmitLogin').text('登入');
+						$('#SubmitLogin').attr('disabled',false);
+					}
+					else
+					{
+						swal({
+						  title: '登入失敗',
+						  text: '系統發生錯誤',
+						  type: 'error',
+						  confirmButtonClass: "btn btn-info",
+						  buttonsStyling: false
+						});
+						$('#SubmitLogin').text('登入');
+						$('#SubmitLogin').attr('disabled',false);
+					}
+				},
+				error: function (xhr, ajaxOptions, thrownError) {
+					console.log(xhr.status);
+					console.log(thrownError);
+				}
+			});
+		}
+		else
+		{
+			swal({
+			  title: '取消',
+			  text: '輸入格式錯誤',
+			  type: 'error',
+			  confirmButtonClass: "btn btn-info",
+			  buttonsStyling: false
+			})
+		}
+	});
+	
+	$(document).on('click', "#LogoutButton", function() {
+		console.log("aaa");
+		swal({
+			title: '你確定?',
+			text: '你確定要登出嗎?',
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonText: '確定',
+			cancelButtonText: '取消',
+			confirmButtonClass: "btn btn-success",
+			cancelButtonClass: "btn btn-danger",
+			buttonsStyling: false
+		}).then(function() {
+			$.ajax({
+				type: "POST",
+				url: "./assets/inc/auth.php",
+				data: {	logout: "logout"},
+				success:swal({
+					title: '成功',
+					text: '成功登出.',
+					type: 'success',
+					confirmButtonClass: "btn btn-success",
+					buttonsStyling: false
+				}).then(function () {
+					window.location.href="./index.php";
+				}),
+				error: function (xhr, ajaxOptions, thrownError) {
+					console.log(xhr.status);
+					console.log(thrownError);
+				}
+			});
+		}, function(dismiss) {
+			if (dismiss === 'cancel') {
+				swal({
+				  title: '取消',
+				  text: '你沒有登出 :)',
+				  type: 'error',
+				  confirmButtonClass: "btn btn-info",
+				  buttonsStyling: false
+				})
+			  }
+		})
+	});
