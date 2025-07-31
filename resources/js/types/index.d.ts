e5d89bd19e62563c80ba567fe0835ec57bbf7860
Kento <rogeraabbccdd@gmail.com>
2025-07-31 08:56:59 +0800

    建立專案

---
+++ resources/js/types/index.d.ts
@@ -0,0 +1,38 @@
+import type { LucideIcon } from 'lucide-vue-next';
+import type { Config } from 'ziggy-js';
+
+export interface Auth {
+    user: User;
+}
+
+export interface BreadcrumbItem {
+    title: string;
+    href: string;
+}
+
+export interface NavItem {
+    title: string;
+    href: string;
+    icon?: LucideIcon;
+    isActive?: boolean;
+}
+
+export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
+    name: string;
+    quote: { message: string; author: string };
+    auth: Auth;
+    ziggy: Config & { location: string };
+    sidebarOpen: boolean;
+};
+
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    avatar?: string;
+    email_verified_at: string | null;
+    created_at: string;
+    updated_at: string;
+}
+
+export type BreadcrumbItemType = BreadcrumbItem;
