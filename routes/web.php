bbeb08422280a7e2371af5ed55a10f4ff0289e5f
Kento <rogeraabbccdd@gmail.com>
2025-08-03 17:53:44 +0800

    設定基本路由

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,30 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    if (Auth::check()) {
+        return redirect()->route('stores');
+    } else {
+        return redirect()->route('login');
+    }
 })->name('home');
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+
+    Route::get('stores', function () {
+        return Inertia::render('stores/Index');
+    })->name('stores');
+});
+
