373cee4a6cba37618ac70db2e4595ad5012f62e4
Kento <rogeraabbccdd@gmail.com>
2025-08-07 10:13:29 +0800

    補 font awesome

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,6 +6,7 @@ import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
 
 import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
 import '@quasar/extras/roboto-font/roboto-font.css';
 import { Quasar, Notify, Dialog } from 'quasar';
 import quasarIconSet from 'quasar/icon-set/svg-material-icons';
@@ -23,6 +24,9 @@ const quasarConfig = {
     },
     lang: quasarLang,
     iconSet: quasarIconSet,
+    extras: [
+        'fontawesome-v6',
+    ]
 }
 
 createInertiaApp({
