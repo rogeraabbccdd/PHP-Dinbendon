0f4219d04dfcd8aa1d9b36217e1b2c90db89770b
Kento <rogeraabbccdd@gmail.com>
2025-08-11 10:11:03 +0800

    修改店家卡片已歇業狀態文字顏色

--- resources/js/components/StoreCard.vue
+++ resources/js/components/StoreCard.vue
@@ -3,7 +3,7 @@ q-card.card-store
     //- 卡片圖片
     q-img(:src="props.image" height="25vh")
         .absolute-center.full-height.full-width.column.justify-center.align-center(v-if="props.is_closed")
-            .text-h4.text-center 已歇業
+            .text-h4.text-red-6.text-center 已歇業
     //- 卡片內容
     q-card-section.card-store-title
         //- 店家名稱
