83b34f0ea00c3deeb7f683f8011732ea9bfac42c
Kento <rogeraabbccdd@gmail.com>
2026-02-09 22:27:07 +0800

    trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
